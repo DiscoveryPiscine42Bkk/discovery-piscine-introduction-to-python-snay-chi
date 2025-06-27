7cb0ed53799528e489d2c2ada68d444fa35ebe16
snay-chi <soenaychi175@gmail.com>
2025-06-27 10:03:29 +0630

    Update advanced_mult.py

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -1,1 +1,15 @@
+import sys
+
+if len(sys.argv) > 1:
+    print("none")
+else:
+    i = 0
+    while i <= 10:
+        j = 0
+        row = f"Table de {i}:"
+        while j <= 10:
+            row += f" {i * j}"
+            j += 1
+        print(row)
+        i += 1
 
