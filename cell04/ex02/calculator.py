86af5502e62c2fb9a7edd80cdcd952c066bc2aac
snay-chi <soenaychi175@gmail.com>
2025-06-27 15:45:44 +0630

    Update calculator.py

--- cell04/ex02/calculator.py
+++ cell04/ex02/calculator.py
@@ -1,1 +1,12 @@
+def main():
+    num1 = float(input("Give me the first number: "))
+    num2 = float(input("Give me the second number: "))
+    print("Thank you!")
+    print(f"{int(num1)} + {int(num2)} = {int(num1 + num2)}")
+    print(f"{int(num1)} - {int(num2)} = {int(num1 - num2)}")
+    print(f"{int(num1)} / {int(num2)} = {int(num1 / num2)}")
+    print(f"{int(num1)} * {int(num2)} = {int(num1 * num2)}")
+
+if __name__ == "__main__":
+    main()
 
