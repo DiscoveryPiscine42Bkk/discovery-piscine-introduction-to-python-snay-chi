3e1fc220b67876ed204d858690c176dd29a0e385
snay-chi <soenaychi175@gmail.com>
2025-06-27 21:07:32 +0630

    Update parameters.py

--- cell05/ex04/parameters.py
+++ cell05/ex04/parameters.py
@@ -1,1 +1,9 @@
+import sys
+
+def main():
+    num_params = len(sys.argv) - 1  # Exclude the script name itself
+    print(f"Number of parameters: {num_params}.")
+
+if __name__ == "__main__":
+    main()
 
