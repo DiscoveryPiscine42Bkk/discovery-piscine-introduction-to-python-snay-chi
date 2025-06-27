cc81ebea908ab257b8c093a18f0edef8e60910aa
snay-chi <soenaychi175@gmail.com>
2025-06-27 10:38:03 +0630

    Update i_got_that.py

--- cell03/ex02/i_got_that.py
+++ cell03/ex02/i_got_that.py
@@ -1,6 +1,12 @@
-while True:
-    user_input = input()
-    if user_input == "STOP":
-        break
-    print("I got that! Anything else?")
+def main():
+    while True:
+        user_input = input("What you gotta say? : ") if not 'got_any' in locals() else input("I got that! Anything else? : ")
+        # Normalize input to check for "STOP"
+        if user_input == "STOP":
+            break
 
+        # After first iteration, we prompt differently
+        got_any = True
+
+if __name__ == "__main__":
+    main()
